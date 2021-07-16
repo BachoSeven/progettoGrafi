85602954e10af3508c71b6ad4a9f31afa13ed63e
Francesco Minnocci <ad17fmin@uwcad.it>
2021-07-16 16:23:31 +0200

    Add config.mk

--- Makefile
+++ Makefile
@@ -1,6 +1,16 @@
+include config.mk
+
+SRC = grafo.c
+
 all: script grafo
 
+script:
+	./filtro.sh
+
 grafo:
+	${CC} ${SRC} -o ${SRC%.c}
 
 clean:
-	rm -rf *.txt grafo
+	rm -rf txt/*.txt grafo
+
+.PHONY: all script grafo clean
--- filtro.sh
+++ filtro.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+# aria2c https://datasets.imdbws.com/name.basics.tsv.gz
+# aria2c https://datasets.imdbws.com/title.principals.tsv.gz
+# aria2c https://datasets.imdbws.com/title.basics.tsv.gz
+
 ## Estraggo i film non adulti; salvo identificativi e nomi
 zcat title.basics.tsv.gz | tail -n +2 | awk -F'\t' '$5 != 1' | cut -f1,3 > txt/FilmNonAdulti.txt
 
 ## Estraggo gli attori/attrici; salvo identificativi e nomi
-zcat name.basics.tsv.gz | tail -n +2 | grep -E "actor|actress" | cut -f1,2 > txt/Attorə.txt
+zcat name.basics.tsv.gz | grep -E "actor|actress" | cut -f1,2 > txt/Attorə.txt
 
 ## Estraggo gli attori/attrici; salvo identificativi film-attore
-zcat title.principals.tsv.gz | tail -n +2 | grep -E "actor|actress" | cut -f1,3 > txt/Relazioni.txt
+zcat title.principals.tsv.gz | grep -E "actor|actress" | cut -f1,3 > txt/Relazioni.txt
+
+nodi="$(wc -l txt/Attorə.txt)"
