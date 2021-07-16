f0821918b7038ab8c74d11de3d002e22ec48e688
Francesco Minnocci <ad17fmin@uwcad.it>
2021-07-16 19:51:39 +0200

    Fix curl command(add resume flag); add `awk` command to filter Relazioni

--- filtro.sh
+++ filtro.sh
@@ -2,7 +2,7 @@
 
 links="https://datasets.imdbws.com/name.basics.tsv.gz https://datasets.imdbws.com/title.principals.tsv.gz https://datasets.imdbws.com/title.basics.tsv.gz"
 for l in $links; do
-	curl -O $l &
+	curl -O $l -C - &
 done; wait
 
 ## Estraggo i film non adulti; salvo identificativi e nomi
@@ -13,5 +13,8 @@ zcat name.basics.tsv.gz | grep -E "actor|actress" | cut -f1,2 > txt/Attorə.txt
 
 ## Estraggo gli attori/attrici; salvo identificativi film-attore
 zcat title.principals.tsv.gz | grep -E "actor|actress" | cut -f1,3 > txt/Relazioni.txt
+
+## Rimuovo i film adulti da Relazioni.txt, utilizzando FilmNonAdulti.txt
+awk 'FNR==NR { A[$1]; next } $1 in A' txt/FilmNonAdulti.txt txt/Relazioni.txt > txt/Relazioni-filtrate.txt
 
 nodi="$(wc -l txt/Attorə.txt)"
