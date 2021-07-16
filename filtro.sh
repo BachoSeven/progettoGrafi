d113c9d8e23c1ac9c0acfe983e6c57876c0182d1
Francesco Minnocci <ad17fmin@uwcad.it>
2021-07-17 01:46:44 +0200

    Refactor script, adding `sed` to prettify output txts

--- filtro.sh
+++ filtro.sh
@@ -3,23 +3,40 @@
 links="https://datasets.imdbws.com/name.basics.tsv.gz https://datasets.imdbws.com/title.principals.tsv.gz https://datasets.imdbws.com/title.basics.tsv.gz"
 
 for l in $links; do
-	# -O	: scrivi su file con nome dato dal server
-	# -C -: continua da dove eri rimasto
-	# &		: esegui i download in background --> paralleli
-	curl -O $l -C - &
+	curl \
+		# scrivi su file con nome dato dal server
+		-O   \
+		# continua da dove eri rimasto
+		-C - \
+		# esegui i download in background --> paralleli
+		$l &
 done; wait
 
-## Estraggo i film non adulti
-## Salvo identificativi e nomi dei film
-zcat title.basics.tsv.gz | tail -n +2 | awk -F'\t' '$5 != 1' | cut --output-delimiter=' ' -f1,3 > txt/FilmNonAdulti.txt
+zcat title.basics.tsv.gz |\
+	tail -n +2 |\
+	# Estraggo i film non adulti
+	awk -F'\t' '$5 != 1' |\
+	# Salvo identificativi e nomi dei film
+	cut --output-delimiter=' ' -f1,3 |\
+	# Utilizzo `sed` per rendere l'output più leggibile
+	sed 's/^tt//g;s/^0*//g' > txt/FilmNonAdulti.txt
 
-## Estraggo solo gli attori/attrici
-## Salvo identificativi ed i nomi
-zcat name.basics.tsv.gz | grep -E "actor|actress" | cut --output-delimiter=' ' -f1,2 > txt/Attorə.txt
+zcat name.basics.tsv.gz |\
+	# Estraggo solo gli attori/attrici
+	grep -E "actor|actress" |\
+	# Salvo identificativi ed i nomi
+	cut --output-delimiter=' ' -f1,2 |\
+	# Utilizzo `sed` per rendere l'output più leggibile
+	sed 's/^nm//g;s/^0*//g' > txt/Attorə.txt
 
-## Estraggo gli attori/attrici da title.principals
-## Salvo identificativi film-attore
-## Infine, rimuovo i film adulti pure da qui, utilizzando FilmNonAdulti.txt
-zcat title.principals.tsv.gz | grep -E "actor|actress" | cut --output-delimiter=' ' -f1,3 | awk 'FNR==NR { A[$1]; next } $1 in A' txt/FilmNonAdulti.txt - > txt/Relazioni.txt
+zcat title.principals.tsv.gz |\
+	# Estraggo gli attori/attrici da title.principals
+	grep -E "actor|actress" |\
+	# Salvo identificativi film-attore
+	cut --output-delimiter=' ' -f1,3 |\
+	# Rimuovo i film adulti pure da qui, utilizzando FilmNonAdulti.txt
+	awk 'FNR==NR { A[$1]; next } $1 in A' txt/FilmNonAdulti.txt - |\
+	# Utilizzo `sed` per rendere l'output più leggibile
+	sed 's/^tt//g;s/^0*//g' > txt/Relazioni.txt
 
 nodi="$(wc -l txt/Attorə.txt)"
