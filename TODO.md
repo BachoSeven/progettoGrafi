a06443f1a0c7dde6a521ade0dd7f9b256c21758f
Francesco Minnocci <ad17fmin@uwcad.it>
2021-08-09 00:22:36 +0200

    Update TODO.md, add betweenness calculation to geom_sample()

--- TODO.md
+++ TODO.md
@@ -1,5 +1,5 @@
 ## Problemi
-- Mantenere gli identificativi degli attori mentre vengono salvati nel grafo --> struct?
-- Problema inverso di quello sopra: come assegnare numeri crescenti a partire da 1 agli attori presenti in Relazioni?
-- Grossi sosteneva che #{archi} fosse approssimativamente 8 milioni, ovvero #{Film}, ma ogni film crea potenzialmente binom(#{attoripresentinelfilm},2) archi(se non ridondanti), per cui dovrebbe essere un bel po' più alto(anche se come ordine di grandezza potrebbe starci in effetti...)
-- Come costruire estrapolare gli archi in modo non ridondante da Relazioni.txt?
+- [x] Mantenere gli identificativi degli attori mentre vengono salvati nel grafo --> struct?
+- [x] Come costruire estrapolare gli archi in modo non ridondante da Relazioni.txt?
+- ~~Problema inverso di quello sopra: come assegnare numeri crescenti a partire da 1 agli attori presenti in Relazioni?~~
+- ~~Grossi sosteneva che #{archi} fosse approssimativamente 8 milioni, ovvero #{Film}, ma ogni film crea potenzialmente binom(#{attoripresentinelfilm},2) archi(se non ridondanti), per cui dovrebbe essere un bel po' più alto(anche se come ordine di grandezza potrebbe starci in effetti...)~~
--- grafo.cpp
+++ grafo.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <queue>
+#include <list>
+#include <stack>
 #include <fstream> // getline
 #include <algorithm> // find
-#include <queue>	// coda
 #include <math.h> // ceil
 #include <sys/time.h> // per gettimeofday
 
@@ -29,6 +31,10 @@ int Dist[MAXN];
 // per approx
 float _clos[MAXN];
 float _harm[MAXN];
+float _betw[MAXN];
+int _sigma[MAXN];
+float _delta[MAXN];
+list<int> Pred[MAXN];
 float _lin[MAXN];
 int S; // grandezza componenti
 
@@ -82,12 +88,12 @@ void buildG()
 				next=stoi(t.substr(0, t.find(del)));
 			}
 
-			for (auto primo = filmcrew.begin(); primo != filmcrew.end(); ++primo) {
-				for (auto secondo = primo + 1; secondo != filmcrew.end(); ++secondo) {
+			for(auto primo=filmcrew.begin(); primo!=filmcrew.end(); ++primo) {
+				for(auto secondo=primo+1; secondo!=filmcrew.end(); ++secondo) {
 					u=*primo;
 					v=*secondo;
 					// controllo di non star duplicando un arco
-					if( find(adj[u].begin(), adj[u].end(), v) == end(adj[u]) ) { // find è O(n) con n=lunghezza lista di adiacenza
+					if(find(adj[u].begin(), adj[u].end(), v) == end(adj[u])) { // find è O(n) con n=lunghezza lista di adiacenza
 						adj[u].push_back(v);
 						adj[v].push_back(u);
 					}
@@ -102,7 +108,7 @@ void buildG()
 
 void printG()
 {
-	for(int u=1;u<N;++u) {
+	for(int u=1; u<N; ++u) {
 		// Stampa lista di adiacenza solo se non è vuota.
 		if(!adj[u].empty()) {
 			cout << u << ": ";
@@ -168,7 +174,6 @@ void geom_exact(int s,int S)
 	// cout << (float)(S^2)*clos << " ";
 }
 
-// TODO: implementa betwenness assieme a queste (slide+paper, https://www.cl.cam.ac.uk/teaching/1617/MLRD/handbook/brandes.html)
 void geom_sample(vector<int> sample, vector<int> comp)
 {
 	for(auto s:sample) {
@@ -179,10 +184,13 @@ void geom_sample(vector<int> sample, vector<int> comp)
 		Q.push(s);
 		visitato[s]=true;
 		Dist[s]=0;
+		stack<int> _S;
+		_sigma[s]=1;
 
 		while(!Q.empty()) {
 			auto u=Q.front();
 			Q.pop();
+			_S.push(u);
 			for(auto v:adj[u]) {
 				if(!visitato[v]) {
 					Q.push(v);
@@ -191,6 +199,22 @@ void geom_sample(vector<int> sample, vector<int> comp)
 					_clos[v]+=Dist[v];
 					_harm[v]+=1./Dist[v];
 				}
+				// Abbiamo già incontrato u, e fornisce un SP per v
+				if(Dist[v] == Dist[u]+1) {
+					_sigma[v]+=_sigma[u];
+					Pred[v].push_back(u);
+				}
+			}
+		}
+		while(!_S.empty()) {
+			auto t=_S.top();
+			_S.pop();
+			for(auto i=Pred[t].begin(); i!=Pred[t].end(); ++i) {
+					int w=*i;
+					_delta[w]+=((float)_sigma[w]/(double)_sigma[t])*(1.+_delta[t]);
+			}
+			if(t!=s) {
+					_betw[t]+=_delta[t];
 			}
 		}
 	}
@@ -199,7 +223,7 @@ void geom_sample(vector<int> sample, vector<int> comp)
 void printTop(vector<int> Cc)
 {
 	priority_queue<pair<float,int>> P,Q;
-	int m=100;
+	int m=10;
 	for (auto i:Cc) {
 		// O(S*log(m)) tramite heap min
 		if(P.size()<(unsigned)m) {
@@ -222,7 +246,7 @@ void printTop(vector<int> Cc)
 		int __i=Q.top().second;
 		Actor p = *(find_if(A.begin(), A.end(), [_i](const Actor &a) { return a.id == _i; }));
 		Actor q = *(find_if(A.begin(), A.end(), [__i](const Actor &a) { return a.id == __i; }));
-		if(i==0)
+		if(i == 0)
 			cout << "1 🏅" << "\t| " << p.name << "\t\t\t| " << q.name << endl;
 		else
 			cout << i+1 << "\t| " << p.name << "\t\t\t| " << q.name << endl;
@@ -239,7 +263,7 @@ int main()
 	cin >> N;
 	// N++;
 
-	readNames();
+	// readNames();
 
 	buildG();
 
@@ -254,10 +278,8 @@ int main()
 			vector<int> Cc=componente(i);
 			int S=Cc.size();
 			if(S>1000) {
-				// delta=0.1; eps=0.5 (k=139)
-				// int k=ceil(2*(4)*(log(2)+log(S)+log(10)));
-				// delta=0.05; eps=0.4 (k=225)
-				int k=ceil(2*(6.25)*(log(2)+log(S)+log(20)));
+				// eps=0.3, delta=0.01(probablity >=99%)(k=436)
+				int k=ceil(2*(1/0.09)*(log(2)+log(S)+log(100)));
 				vector<int> sample;
 				for (int i=0; i<k; ++i) {
 					sample.push_back(Cc[(rand() % S)]);
@@ -265,6 +287,10 @@ int main()
 				for(auto i:Cc) {
 					_harm[i]=0;
 					_clos[i]=0;
+					_betw[i]=0;
+					_sigma[i]=0;
+					_delta[i]=0;
+					_lin[i]=0;
 				}
 
 				// struct timeval beg, end;
@@ -276,8 +302,8 @@ int main()
 				// freopen("txt/centrality.txt", "a", stdout);
 				// for(auto j:Cc) {
 					// // _lin[j]=(float)(S^2)*_clos[j];
-					// // sintassi: nodo: grado closeness harmonic
-					// cout << j << ": " << adj[j].size() << " " << (((float)(S-1)*(float)k)/((float)S*_clos[j])) << " " << ((float)S*_harm[j])/((float)(S-1)*(float)k) << endl;
+					// // sintassi: nodo: grado closeness harmonic betweenness
+					// cout << j << ": " << adj[j].size() << " " << (((float)(S-1)*(float)k)/((float)S*_clos[j])) << " " << ((float)S*_harm[j])/((float)(S-1)*(float)k) << " " << _betw[j]/((float)S*(float)(S-1))  << endl;
 				// }
 				// cout << endl;
 				// fclose(stdout);
@@ -285,7 +311,9 @@ int main()
 				// geom_exact(306,S);
 				// geom_exact(616,S);
 
+				// freopen("TOP.txt","w",stdout);
 				printTop(Cc);
+				// fclose(stdout);
 
 				sample.clear();
 			}
